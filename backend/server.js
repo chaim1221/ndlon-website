9fd5b4828727e1769b8d563a23eff6619722decd
Chaim Eliyah <chaimeliyah@gmail.com>
2018-03-18 16:13:20 -0700

    add production option [skip ci]

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require('./app.js');
+const port = process.argv[2] == "--production" ? 80 : 8080;
 
-app.listen(8080, function() {
-  console.log('Server running on port 8080.');
+app.listen(port, function() {
+  console.log('Server running on port ' + port + '.');
 });
